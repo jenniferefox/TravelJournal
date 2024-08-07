f0391ee04ab80925d37074800e54c2af69dafaa5
Jennifer Fox <jenniferelizfox@gmail.com>
2024-08-07 17:46:47 +0100

    Journal data created
    Currently one entry is repeated for testing purposes

---
+++ traveljournal/src/data.js
@@ -0,0 +1,31 @@
+import nata from './assets/nata.png';
+import corgi from './assets/corgi.jpg';
+
+export default [
+  { id: 1,
+    location: "LISBON",
+    title: "HomeCooking Lisbon",
+    photo: nata,
+    maps_url: "https://maps.app.goo.gl/ocyJWUeYg2FHHfkr8",
+    timerange: "August 2024",
+    details: "Joined a traditional Portuguese cooking class and prepared a delicious 4 course meal with the help of local chefs. The dessert, pastel de nata, was not to be missed!"
+  },
+  { id: 2,
+    location: "LISBON",
+    title: "Corgi spotted",
+    photo: corgi,
+    maps_url: "https://maps.app.goo.gl/ZhkiiR5HqZQ4WgSV9",
+    timerange: "August 2024",
+    details: "Spotted a hot and bothered corgi on the Praça do Comércio, one of the largest squares in Lisbon."
+  },
+  { id: 3,
+    location: "LISBON",
+    title: "HomeCooking Lisbon",
+    photo: nata,
+    maps_url: "https://maps.app.goo.gl/ocyJWUeYg2FHHfkr8",
+    timerange: "August 2024",
+    details: "Joined a traditional Portuguese cooking class and prepared a delicious 4 course meal with the help of local chefs. The dessert, pastel de nata, was not to be missed!"
+  }
+
+
+]
