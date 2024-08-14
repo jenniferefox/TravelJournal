2083e4c271c4a6b26a80ee74fb52a2fba8818560
Jennifer Fox <jenniferelizfox@gmail.com>
2024-08-14 01:38:02 +0100

    Travel Journal updated
    Entries added: Cascais Beach, National Tile Museum

--- traveljournal/src/data.js
+++ traveljournal/src/data.js
@@ -1,5 +1,7 @@
 import nata from './assets/nata.png';
 import corgi from './assets/corgi.jpg';
+import beach from './assets/beach.jpg';
+import tiles from './assets/tiles.jpeg';
 
 export default [
   { id: 1,
@@ -20,12 +22,19 @@ export default [
   },
   { id: 3,
     location: "LISBON",
-    title: "HomeCooking Lisbon",
-    photo: nata,
-    maps_url: "https://maps.app.goo.gl/ocyJWUeYg2FHHfkr8",
+    title: "Cascais",
+    photo: beach,
+    maps_url: "https://maps.app.goo.gl/paSaosfQbpXSRKbH8",
     timerange: "August 2024",
-    details: "Joined a traditional Portuguese cooking class and prepared a delicious 4 course meal with the help of local chefs. The dessert, pastel de nata, was not to be missed!"
+    details: "Spent some time at the beach in Cascais to cool off."
+  },
+  { id: 4,
+    location: "LISBON",
+    title: "The National Tile Museum",
+    photo: tiles,
+    maps_url: "https://maps.app.goo.gl/DrE5e4P5VCExEW9n9",
+    timerange: "August 2024",
+    details: "The tile museum painted a rich history of Portuguese tiles over the last 1000+ years. It was a beautiful place, and the inviting aircon provided a safe haven from the heat."
   }
-
 
 ]
